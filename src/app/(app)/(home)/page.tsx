1450445f04f30098990ee0c9dbf4454a34107310
Underprotection <mertcandemirkaya@outlook.com>
2025-05-13 14:12:11 +0300

    Authentication

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
   const trpc = useTRPC();
-  const categories = useQuery(trpc.categories.getMany.queryOptions());
+  const { data } = useQuery(trpc.auth.session.queryOptions());
 
-  return <div className="p-4">{JSON.stringify(categories.data, null, 2)}</div>;
+  return <div className="p-4">{JSON.stringify(data?.user, null, 2)}</div>;
 }
