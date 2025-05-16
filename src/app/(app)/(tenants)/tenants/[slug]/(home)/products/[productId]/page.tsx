dba9c02da9740ff16611a4f6fe2d023c28290ce5
Underprotection <mertcandemirkaya@outlook.com>
2025-05-16 20:14:02 +0300

    Product Page

---
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
@@ -0,0 +1,26 @@
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { ProductView } from "@/modules/products/ui/views/product-view";
+
+interface ProductPageProps {
+  params: Promise<{ productId: string; slug: string }>;
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const { productId, slug } = await params;
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.tenants.getOne.queryOptions({
+      slug,
+    })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductView productId={productId} tenantSlug={slug} />
+    </HydrationBoundary>
+  );
+}
